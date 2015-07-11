8f71e3435457e077d63f2532efcaf1b00cc8cef9
Josef Antoni <antoni.josef@icloud.com>
2015-07-11 21:09:15 +0200

    Fixed position of items. Working properly in simulator.

--- WeatherCheck/AppDelegate.swift
+++ WeatherCheck/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         //status bar set to white style
-        UIApplication.sharedApplication().setStatusBarStyle(UIStatusBarStyle.LightContent, animated: true)
+        UIApplication.sharedApplication().setStatusBarStyle(UIStatusBarStyle.LightContent, animated: false)
         return true
     }
 
