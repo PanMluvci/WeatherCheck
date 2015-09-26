cae49c7b75ace931796b29523121f5c34385a243
Josef Antoni <antoni.josef@icloud.com>
2015-09-26 15:38:09 +0200

    Main screen, scroll panel finally works according to auto layout rules.

--- WeatherCheck/ViewController.swift
+++ WeatherCheck/ViewController.swift
@@ -150,14 +150,14 @@ class ViewController: UIViewController {
         self.navigationController!.toolbar.barTintColor = UIColor(red: 117/255, green:209/255, blue: 255/255, alpha: 1)
         self.navigationController!.toolbar.layer.borderWidth = 0.5
         self.navigationController!.toolbar.layer.borderColor = UIColor.whiteColor().CGColor
-       /*
-        self.backgroundImageView.backgroundColor = UIColor(red: 117/255, green:209/255, blue: 255/255, alpha: 1)
+       
+       // self.backgroundImageView.backgroundColor = UIColor(red: 117/255, green:209/255, blue: 255/255, alpha: 1)
         
-        srollingInfoLabel.contentSize.height = 150
+        srollingInfoLabel.contentSize.height = 450
         
-        descriptionLabel.layer.borderWidth = 0.5
-        descriptionLabel.layer.borderColor = UIColor.whiteColor().CGColor
-        */
+        //descriptionLabel.layer.borderWidth = 0.5
+        //descriptionLabel.layer.borderColor = UIColor.whiteColor().CGColor
+
     }
     
     override func preferredStatusBarStyle() -> UIStatusBarStyle {
